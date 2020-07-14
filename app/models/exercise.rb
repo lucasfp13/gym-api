bceb0392340b270ba7993658ed8170161603e3e9
Lucas Portela <lucasfjportela@gmail.com>
2020-07-14 01:10:44 -0300

    Added some exercise model's validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,6 @@
 class Exercise < ApplicationRecord
   has_one_attached :image
 
-  validates :name, :calories, :duration, presence: true
+  validates :name, presence: true
+  validates :calories, :duration, presence: true, numericality: { greater_than: 0 }
 end
